47befea8574da88c293010d471c96b58ba5fce84
Richard Nwokoye <RNwokoye@gmail.com>
2021-05-27 19:57:30 -0400

    add changes for data

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 CORS(app)
 
 # Use pyMongo to establish mongo database connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/CountryData")
+mongo = PyMongo(app, uri="mongodb://localhost:27017/CountryData5")
 
 
 # index route
@@ -24,12 +24,12 @@ def index():
 # create route that renders index page
 @app.route("/data")
 def data():
-    cur_data = mongo.db.countries.find({'Year': 2017})
-    RESULTS = {'Country': []}
-    data = [i for i in cur_data]
-    res = pd.DataFrame(data).fillna(0)
-    res2 = res.to_dict('records')
-    for j in res2:
+    RESULTS = {'Country': [], "Stats": []}
+    country_data = mongo.db.countries.find({'Year': 2017})
+    data2 = [i for i in country_data]
+    res2 = pd.DataFrame(data2).fillna(0)
+    res3 = res2.to_dict('records')
+    for j in res3:
         RESULTS['Country'].append({
             'name': j.get('Country'),
             'id': str(j.get('Country_Code')),
@@ -41,8 +41,18 @@ def data():
             'RT_FoodScore': j.get('IncomeAdjusted_LMY_Right_to_Food_Score'),
             'RT_WorkScore': j.get('IncomeAdjusted_LMY_Right_to_Work_Score'),
             'GDP_per_capita_': j.get('GDP_per_capita_(2011_PPP$)_for_Most_Recent_Observation_on_Net_Secondary_Enrollment')
+        }),
+        RESULTS['Stats'].append({
+            'name': j.get('Country'),
+            'quality_of_life_score': j.get('IncomeAdjusted_LMY_Quality_Of_Life_Score'),
+            'education_score': j.get('IncomeAdjusted_LMY_Right_to_Education_Score'),
+            'health_score': j.get('IncomeAdjusted_LMY_Right_to_Health_Score'),
+            'housing_score': j.get('IncomeAdjusted_LMY_Right_to_Housing_Score'),
+            'food_score': j.get('IncomeAdjusted_LMY_Right_to_Food_Score'),
+            'work_score': j.get('IncomeAdjusted_LMY_Right_to_Work_Score'),
+            'GDP_per_capita': j.get('GDP_per_capita_(2011_PPP$)_for_Most_Recent_Observation_on_Net_Secondary_Enrollment')
+            
         })
-
     return jsonify(RESULTS)
 
 
